7403ecccca3b1f023d219735898447f050eb8f4c
Elektritakso <48553540+Elektritakso@users.noreply.github.com>
2019-09-23 09:46:22 +0300

    Cruel world test commit

--- print/src/Hello.java
+++ print/src/Hello.java
@@ -2,5 +2,9 @@ public class Hello {
     public static   void    main(String[] args) {
         System.out.println("hello world");
         System.out.println("Goodbye cruel world!");
+        // this is a random comment for learning purposes
+        System.out.print("This is a ");
+        System.out.print("Massiv hole"); // print some more stuff here, we combined two sentences
+        //Ignore this  System.out.print("Gooing to ignore this stuff right here");
     }
 }

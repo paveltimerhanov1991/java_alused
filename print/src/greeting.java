d6f6305d673cd2267239cb3531e2d9e50977456d
Elektritakso <48553540+Elektritakso@users.noreply.github.com>
2019-09-30 11:35:58 +0300

    Calculating the circumference 10

--- print/src/greeting.java
+++ print/src/greeting.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
-
-public class greeting {
-    public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-
-        System.out.print("Type number: ");
-        int number = Integer.parseInt(reader.nextLine());   // Reads a line from the users keyboard
-
-        System.out.print("Type another number: ");
-        int number2 = Integer.parseInt(reader.nextLine()); // Reads a string variable from the keyboard and transfers it to an integer
-
-        System.out.println("Division: "  + number + " / " + number2 + " = " + (number / number2));
+public class greeting
+{
+    public static void main(String[] args)
+    {
+        int r;
+        double pi = 3.14, area;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Type the radius:");
+        r = s.nextInt();
+        area = pi * r * r;
+        System.out.println("Circumference of the circle: "+area);
     }
 }

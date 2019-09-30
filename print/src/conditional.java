c011f9e469b2006934bb7ac90c9f67dc8aff0dd6
Elektritakso <48553540+Elektritakso@users.noreply.github.com>
2019-09-30 14:32:52 +0300

    E 15

--- print/src/conditional.java
+++ print/src/conditional.java
@@ -4,17 +4,17 @@ public class conditional {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        System.out.print("Type your number: ");
+        System.out.print("How old are you: ");
         int number = Integer.parseInt(reader.nextLine());
 
-        if(number > 0) {
-            System.out.println("This number is positive: ");
+        if(number >= 18) {
+            System.out.println("You have reached the age of majority: ");
     }
-        System.out.print("Type your number again: ");
+        System.out.print("Type your age again: ");
         int number1 = Integer.parseInt(reader.nextLine());
 
-        if(number1 < 0) {
-            System.out.println("Your number is negative: ");
+        if(number1 <= 18) {
+            System.out.println("You have not reached the age of majority: ");
         }
     }
 }

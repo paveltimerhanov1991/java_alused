754cfa3eb2c354b2659341a55882782431e776cb
Elektritakso <48553540+Elektritakso@users.noreply.github.com>
2019-09-30 11:23:18 +0300

    Multiply exercise 7

--- print/src/variables.java
+++ print/src/variables.java
@@ -1,13 +1,15 @@
 public class variables {
     public static void main(String[] args) {
-        int x = 5;
-        int y = 4;
+        int x = 2;
+        int y = 8;
+        y = 277;
+        x = 111;
 
         System.out.print(x);
-        System.out.print( " + ");
+        System.out.print( " * ");
         System.out.print(y);
         System.out.print(" = ");
-        System.out.println(x + y);
+        System.out.println(x * y);
     }
 }
 

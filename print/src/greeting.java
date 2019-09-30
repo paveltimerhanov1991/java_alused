51e49956e7b5c28c63af9daf9631eb5cc7bddbfd
Elektritakso <48553540+Elektritakso@users.noreply.github.com>
2019-09-30 11:28:31 +0300

    Adder exercise 8

--- print/src/greeting.java
+++ print/src/greeting.java
@@ -4,12 +4,12 @@ public class greeting {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        System.out.print("Your name: ");
-        String name = reader.nextLine();   // Reads a line from the users keyboard
+        System.out.print("Type number: ");
+        int number = Integer.parseInt(reader.nextLine());   // Reads a line from the users keyboard
 
-        System.out.print("How old are you: ");
-        int age = Integer.parseInt(reader.nextLine()); // Reads a string variable from the keyboard and transfers it to an integer
+        System.out.print("Type another number: ");
+        int number2 = Integer.parseInt(reader.nextLine()); // Reads a string variable from the keyboard and transfers it to an integer
 
-        System.out.println("Your name is: " + name + ", and you are " + age + " years old, nice to meet you!");
+        System.out.println("Sum of the numbers: "  + (number + number2));
     }
 }

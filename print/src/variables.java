51024d7a7c624ba38189289e0722604da12ef30f
Elektritakso <48553540+Elektritakso@users.noreply.github.com>
2019-09-30 11:15:30 +0300

    Variables exercise 5

--- print/src/variables.java
+++ print/src/variables.java
@@ -1,27 +1,11 @@
 public class variables {
     public static void main(String[] args) {
-        String chickens = "Chickens:";
-        int number = 3;
-        String bacon = "Bacon (kg):";
-        double number1 =  5.5;
-        String tractor = "A tractor: ";
-        String none = "There is none!";
-        number = 9000;
-        number1 = 0.1;
-        none = "Zetor";
-
-
-        System.out.println(chickens);
-        System.out.println(number);
-        System.out.println(bacon);
-        System.out.println(number1);
-        System.out.println(tractor);
-        System.out.println(none);
-        System.out.println();
-        System.out.println("In a nutshell: ");
-        System.out.println(number);
-        System.out.println(number1);
-        System.out.println(none);
+        int seconds =  60;
+        int minutes = 60;
+        int hours = 24;
+        int days = 365;
+        double result = ((seconds * minutes) * hours) * days;
+        System.out.println("There are " + result + "seconds in a year");
     }
 }
 

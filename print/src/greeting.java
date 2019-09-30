bbb21c015acaf02150edd9d9110a6b85a385f573
Elektritakso <48553540+Elektritakso@users.noreply.github.com>
2019-09-30 11:39:20 +0300

    E 11

--- print/src/greeting.java
+++ print/src/greeting.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
-public class greeting
-{
-    public static void main(String[] args)
-    {
-        int r;
-        double pi = 3.14, area;
-        Scanner s = new Scanner(System.in);
-        System.out.print("Type the radius:");
-        r = s.nextInt();
-        area = pi * r * r;
-        System.out.println("Circumference of the circle: "+area);
+
+public class greeting {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type the number: ");
+        int number = Integer.parseInt(reader.nextLine());   // Reads a line from the users keyboard
+
+        System.out.print("Type another number: ");
+        int number2 = Integer.parseInt(reader.nextLine());// Reads a string variable from the keyboard and transfers it to an integer
+
+        int result = Math.max(number, number2);
+        System.out.println("The bigger number is :"   +  result);
     }
 }

03bec2b492945d3e36231349178c0ea2b466b05d
Elektritakso <48553540+Elektritakso@users.noreply.github.com>
2019-09-30 11:43:01 +0300

    E 12

--- print/src/greeting.java
+++ print/src/greeting.java
@@ -4,13 +4,17 @@ public class greeting {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        System.out.print("Type the number: ");
+        System.out.print("Type your Name: ");
+        String name = reader.nextLine();
+        System.out.print("Type your age: ");
         int number = Integer.parseInt(reader.nextLine());   // Reads a line from the users keyboard
 
-        System.out.print("Type another number: ");
-        int number2 = Integer.parseInt(reader.nextLine());// Reads a string variable from the keyboard and transfers it to an integer
+        System.out.print("Type your Name: ");
+        String name1 = reader.nextLine();
+        System.out.print("Type your age: ");
+        int number2 = Integer.parseInt(reader.nextLine());
 
-        int result = Math.max(number, number2);
-        System.out.println("The bigger number is :"   +  result);
+        int result = number + number2;
+        System.out.println(name + " and " + name1 + " are " + result + " years in total");
     }
 }

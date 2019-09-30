8a769508dc56504d6bdc62859b2d8cd4e539006c
Elektritakso <48553540+Elektritakso@users.noreply.github.com>
2019-09-30 10:41:20 +0300

    Variables second commit

--- print/src/variables.java
+++ print/src/variables.java
@@ -1,15 +1,9 @@
 public class variables {
     public static void main(String[] args) {
-        int months = 12;
-        String text = "some text here";
         int wholeNumber = 123;
-        double decimalNumber = 4.5666;
-        boolean isTrue = true;
-
-        System.out.println("The variable's type is text. Its value is " + text);
         System.out.println("The variable's type is integer. Its value is  " + wholeNumber);
-        System.out.println("The variable's type is decimal number. Its value is " + decimalNumber);
-        System.out.println("The variable's type is truth value. Its value is " + isTrue);
+        wholeNumber = 12334;
+        System.out.println("The variable's type is integer. Its value is  " + wholeNumber);
     }
 }
 

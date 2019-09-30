b49b1c2589670a542c16c0901a4f97bdbfba0236
Elektritakso <48553540+Elektritakso@users.noreply.github.com>
2019-09-30 11:08:33 +0300

    Variables exercise 4

--- print/src/variables.java
+++ print/src/variables.java
@@ -1,9 +1,27 @@
 public class variables {
     public static void main(String[] args) {
-        int wholeNumber = 123;
-        System.out.println("The variable's type is integer. Its value is  " + wholeNumber);
-        wholeNumber = 12334;
-        System.out.println("The variable's type is integer. Its value is  " + wholeNumber);
+        String chickens = "Chickens:";
+        int number = 3;
+        String bacon = "Bacon (kg):";
+        double number1 =  5.5;
+        String tractor = "A tractor: ";
+        String none = "There is none!";
+        number = 9000;
+        number1 = 0.1;
+        none = "Zetor";
+
+
+        System.out.println(chickens);
+        System.out.println(number);
+        System.out.println(bacon);
+        System.out.println(number1);
+        System.out.println(tractor);
+        System.out.println(none);
+        System.out.println();
+        System.out.println("In a nutshell: ");
+        System.out.println(number);
+        System.out.println(number1);
+        System.out.println(none);
     }
 }
 

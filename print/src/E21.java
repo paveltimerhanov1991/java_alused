fec878bfe6dcd8c5a0286602175ce26494a19c58
Elektritakso <pavel.timerhanov@khk.ee>
2019-11-25 11:57:40 +0200

    E21

--- print/src/E21.java
+++ print/src/E21.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class E21 {
+    public static void main(String[]args){
+        Scanner reader = new Scanner(System.in);
+
+        int year;
+
+        System.out.print("Type a year: ");
+        year = Integer.parseInt(reader.nextLine());
+
+        if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println("The year is a leap year!");
+        } else {
+            System.out.println("The year is not a leap year!");
+        }
+    }
 }

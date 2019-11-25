f3b08b4acb8df35e9088c2375d4f2c653249ce99
Elektritakso <pavel.timerhanov@khk.ee>
2019-11-25 11:07:10 +0200

    E18

---
+++ print/src/e18.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class e18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter average of your marks (less than 100)::");
+        int average = sc.nextInt();
+        char grade;
+
+        if(average>=50){
+            grade = 'A';
+        }else if(average>=45 && average<49){
+            grade = 'B';
+        }
+        else if(average>=40 && average<44){
+            grade = 'C';
+        }
+        else {
+            grade = 'D';
+        }
+
+        switch(grade) {
+            case 'A' :
+                System.out.println("Excellent!");
+                break;
+            case 'B' :
+            case 'C' :
+                System.out.println("Well done");
+                break;
+            case 'D' :
+                System.out.println("You passed");
+            case 'F' :
+                System.out.println("Better try again");
+                break;
+            default :
+                System.out.println("Invalid grade");
+        }
+        System.out.println("Your grade is " + grade);
+    }
+}

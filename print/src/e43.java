92669b50a28dbc54c6ee8756e02ae9126713b8df
Elektritakso <pavel.timerhanov@khk.ee>
2019-12-01 23:36:09 +0200

    E42+E43

---
+++ print/src/e43.java
@@ -0,0 +1,10 @@
+public class e43 {
+    private static int sum() {
+        return 4 + 3 + 6 + 1;
+    }
+
+    public static void main(String[]args) {
+        int answer = sum();
+        System.out.println("Sum: " + answer);
+    }
+}

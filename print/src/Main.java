d3b9a3dabda91f668b5dc508ec08c7b7cbe3b4af
Elektritakso <pavel.timerhanov@khk.ee>
2019-12-02 13:29:32 +0200

    Multiplier

--- print/src/Main.java
+++ print/src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
-        // You can test your new class here, try e.g.:
 
-        // Product t = new Product("Banana", 1.1, 13);
-        // t.printProduct();
+    public static void main(String[] args) {
     }
 }

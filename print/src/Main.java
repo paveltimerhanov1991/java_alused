12396b4b36a8be4071b21a29821916545d2bffde
Elektritakso <pavel.timerhanov@khk.ee>
2019-12-02 15:15:27 +0200

    Hangman

--- print/src/Main.java
+++ print/src/Main.java
@@ -1,14 +1,8 @@
-import java.util.ArrayList;
-
 public class Main {
-    public static void main(String[] args) {
-        LotteryNumbers lotteryNumbers = new LotteryNumbers();
-        ArrayList<Integer> numbers = lotteryNumbers.numbers();
 
-        System.out.println("Lottery numbers:");
-        for (int number : numbers) {
-            System.out.print(number + " ");
-        }
-        System.out.println("");
+    public static void main(String[] args) {
+        HangmanLogic logic = new HangmanLogic("parameter");
+        HangmanUserInterface game = new HangmanUserInterface(logic);
+        game.start();
     }
 }

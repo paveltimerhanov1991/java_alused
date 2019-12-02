bd849d508b47094141b4e4f26e71a891ebfe824d
Elektritakso <pavel.timerhanov@khk.ee>
2019-12-02 13:25:56 +0200

    Bank Transfer and e69-e71

---
+++ print/src/e69.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class e69 {
+
+    public static boolean palindrome(String text) {
+        if (text.equals(reverseString(text))) {
+            return true;
+        }
+        return false;
+    }
+
+    public static String reverseString(String text) {
+        String reversed = "";
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed += text.charAt(i);
+        }
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Type a text: ");
+        String text = reader.nextLine();
+        if (palindrome(text)) {
+            System.out.println("The text is a palindrome!");
+        } else {
+            System.out.println("The text is not a palindrome!");
+        }
+    }
+}
